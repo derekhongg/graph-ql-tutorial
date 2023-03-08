36d62320c6af9e2d48d5db499c0b830fa9193679
Derek Hong <Derek.Hong@paccar.com>
2023-03-08 15:37:24 -0800

    first commit

---
+++ FakeData.js
@@ -0,0 +1,32 @@
+const UserList = [
+    {
+        id: 1, 
+        name: "John",
+        username: "john",
+        age: 20,
+        nationality: "Canada"
+    },
+    {
+        id: 2, 
+        name: "Sara",
+        username: "sara",
+        age: 22,
+        nationality: "United States of America"
+    },
+    {
+        id: 3, 
+        name: "Andy",
+        username: "andy",
+        age: 19,
+        nationality: "Cambodia"
+    },
+    {
+        id: 4, 
+        name: "Laura",
+        username: "laura",
+        age: 34,
+        nationality: "Brazil"
+    }
+]
+
+module.exports = {UserList};
